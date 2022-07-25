279e8470d333f5e123b2bc885e27c654c901f39b
Ahmed <ahmed@hallajian.co.uk>
2022-07-25 19:22:18 +0100

    experimented with arrays

---
+++ arrays/nameArray.js
@@ -0,0 +1,5 @@
+const names = ["Ahmed", "Theo", "James", "Tim"];
+console.log(names.length);
+console.log(names[1]);
+const newNames = names.concat("Rachel");
+console.log(newNames);
---
+++ arrays/numSum.js
@@ -0,0 +1,7 @@
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let sum = 0;
+array.forEach((number) => {
+  sum += number;
+});
+
+console.log(sum);
